8039c090d250f2b13311c4d63bde282bca4219c4
vkvikaskmr <vkvikaskmr@gmail.com>
2021-11-14 15:06:39 +0000

    add user resource and refactor article index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+    image_tag(image_src, alt: user.username, class: "rounded shadow mx-auto d-block" )
+  end
 end
